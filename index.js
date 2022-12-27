58b2ee0139d8808ca1b06be154ba8d2ddd88ed10
jitujiten <sahuohjitu@gmail.com>
2022-12-27 12:07:35 +0530

    Added the expense tracker final project

--- index.js
+++ index.js
@@ -4,57 +4,78 @@ forminputs.addEventListener("submit",additems);
 
 function additems(e){
 
-let amountinput=document.getElementById("amountinput").value;
-let Descriptioninput=document.getElementById("Descriptioninput").value;
+  e.preventDefault();
 
-let category=document.getElementById("selected").value;
+let amount=e.target.amountinput.value;
+let description=e.target.Descriptioninput.value;
+let category=e.target.selected.value;
 
-if(localStorage.getItem(Descriptioninput)!=null){
-  alert("the description value already exits please change");
-}
 
 let obj={
-  amount:amountinput,
-  Description:Descriptioninput,
-  selected:category
+  amount,
+  description,
+  category
 }
-var objtostring=JSON.stringify(obj);
-localStorage.setItem(obj.Description,objtostring);
+
+axios.post("https://crudcrud.com/api/d03d7abac027498eba8c7b2c664979e6/expensetracker",obj)
+.then((res)=>{
+  showonscreen(res.data);
+})
+.catch((err)=>{
+  console.log(err);
+})
 
 }
 
-for(let i=0;i<localStorage.length;i++){
-   var key=localStorage.key(i); 
-   var data=localStorage.getItem(key);
+window.addEventListener("DOMContentLoaded",()=>{
+
+  axios.get("https://crudcrud.com/api/d03d7abac027498eba8c7b2c664979e6/expensetracker")
+  .then((res)=>{
+    for(var i=0;i<res.data.length;i++){
+      showonscreen(res.data[i]);
+    }
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
    
-   var money=JSON.parse(data).amount;
-   var descript=JSON.parse(data).Description;
-   var purpose=JSON.parse(data).selected;
-   
-   var li=`<li class="list-group-item" id="${descript}"> ${money} ${descript} ${purpose}    <button class="btn btn-danger" onClick="deleting('${descript}')">delete</button>     <button class="btn btn-warning" onClick="editing('${descript}')">edit</button></li>`;
+
+})
+
+
+function showonscreen(obj){
+
+  var li=`<li class="list-group-item" id="${obj._id}"> ${obj.amount} ${obj.description} ${obj.category}    <button class="btn btn-danger" onClick="deleting('${obj._id}')">delete</button>     <button class="btn btn-warning" onClick="editing('${obj.amount}','${obj.description}','${obj.category}','${obj._id}')">edit</button></li>`;
 
    let target=document.getElementById("itemlist");
    target.innerHTML+=li;
 }
 
 
-function deleting(em){
- localStorage.removeItem(em);
- deletefromscreen(em);
+function deleting(userid){
+ 
+  axios.delete(`https://crudcrud.com/api/d03d7abac027498eba8c7b2c664979e6/expensetracker/${userid}`)
+  .then((res)=>{
+    deletefromscreen(userid);
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+   
+
 }
 
-function deletefromscreen(em){
+function deletefromscreen(userid){
   let parent=document.getElementById("itemlist"); 
-  let child=document.getElementById(em);
+  let child=document.getElementById(userid);
   parent.removeChild(child);
 }
 
-function editing(em){
-let amountval=JSON.parse(localStorage.getItem(em)).amount;
-let Descriptionval=JSON.parse(localStorage.getItem(em)).Description;
-let selectedval=JSON.parse(localStorage.getItem(em)).selected;
+function editing(amount,description,selcted,userid){
 
-document.getElementById("amountinput").value=amountval
-document.getElementById("Descriptioninput").value=Descriptionval;
-document.getElementById("selected").value=selectedval;
+document.getElementById("amountinput").value=amount
+document.getElementById("Descriptioninput").value=description;
+document.getElementById("selected").value=selcted;
+
+deleting(userid);
 }
